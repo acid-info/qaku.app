7bc5bc722496f975741ee061c0cecc691a41952b
JulesFILIOT <dev@julesfiliot.com>
2025-02-09 17:37:43 +0100

    chore(PlusIcon): allow to pass down any prop

--- src/components/Icons/PlusIcon/PlusIcon.tsx
+++ src/components/Icons/PlusIcon/PlusIcon.tsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { SVGProps } from 'react'
 
-export const PlusIcon: React.FC = () => (
+export const PlusIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => (
   <svg
     width="14"
     height="14"
     viewBox="0 0 14 14"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path
       d="M11.6673 7.5834H7.58398V11.6667H6.41732V7.5834H2.33398V6.41674H6.41732V2.3334H7.58398V6.41674H11.6673V7.5834Z"
