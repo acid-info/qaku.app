4e45bddd1f6f86e42f06fb9471ddf8852cebcb21
jinhojang6 <jinho5051@gmail.com>
2025-02-17 21:14:57 +0900

    feat: implement user layout

---
+++ src/layouts/DefaultLayout/User.layout.tsx
@@ -0,0 +1,41 @@
+import { Footer } from '@/components/Footer'
+import UserNav from '@/components/Navbar/UserNav'
+import styled from '@emotion/styled'
+import { PropsWithChildren } from 'react'
+
+type Props = PropsWithChildren
+
+export default function UserLayout(props: PropsWithChildren<Props>) {
+  return (
+    <Root>
+      <MainContainer>
+        <UserNav
+          mode={'qna'}
+          title="Town Hall 2025 - New Positions, Updates, And Plans"
+          count={3}
+          id="3212345"
+          onModeChange={(newMode) => console.log(newMode)}
+        />
+        <Main>{props.children}</Main>
+        <Footer />
+      </MainContainer>
+    </Root>
+  )
+}
+
+const Root = styled.div`
+  display: flex;
+  background: var(--black);
+  width: 100%;
+  height: 100vh;
+`
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: calc(100% - var(--footer-height) - var(--header-height));
+`
+
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+`
