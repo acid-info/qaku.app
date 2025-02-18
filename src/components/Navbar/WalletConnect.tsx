0ae47f2e7b0c948280efec96aa920db201acc97d
jinhojang6 <jinho5051@gmail.com>
2025-02-19 02:41:14 +0900

    feat: implement jotai isAuthorized state

--- src/components/Navbar/WalletConnect.tsx
+++ src/components/Navbar/WalletConnect.tsx
@@ -1,11 +1,20 @@
 import styled from '@emotion/styled'
+import { useAtom } from 'jotai'
+import { isAuthorizedAtom } from '../../../atoms/navbar/isAuthorizedAtom'
 import { Button } from '../Button'
 import { LinkIcon } from '../Icons/LinkIcon'
 
 const WalletConnect = () => {
+  const [isAuthorized, setIsAuthorized] = useAtom(isAuthorizedAtom)
+
+  const handleClick = () => {
+    setIsAuthorized((prev) => !prev)
+  }
+
   return (
     <Container>
-      <Button>Connect Wallet</Button>
+      {!isAuthorized && <Button onClick={handleClick}>Connect Wallet</Button>}
+
       <Button icon={<LinkIcon />}>Button</Button>
     </Container>
   )
