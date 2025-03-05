0d93189928bfbab6741eb6aa8cfb0471e75ba9cd
JulesFILIOT <dev@julesfiliot.com>
2025-03-05 12:45:59 +0100

    chore: clean useless/unused code

--- src/components/ApiSubscriptionManager/ApiSubscriptionManager.tsx
+++ src/components/ApiSubscriptionManager/ApiSubscriptionManager.tsx
@@ -1,22 +1,13 @@
 import { apiConnector } from '@/lib/api/connector'
 import { ApiMessageType } from '@/lib/api/types'
-import {
-  AnswerType,
-  PollOptionType,
-  PollType,
-  QuestionType,
-} from '@/types/qna.types'
+import { PollOptionType, PollType } from '@/types/qna.types'
 import { useSetAtom } from 'jotai'
 import { useEffect } from 'react'
-import { answersRecordAtom } from '../../../atoms/answerAtom'
 import { pollsRecordAtom } from '../../../atoms/pollAtom'
 import { pollOptionsRecordAtom } from '../../../atoms/pollOptionAtom'
 import { qnasRecordAtom } from '../../../atoms/qnaAtom'
-import { questionsRecordAtom } from '../../../atoms/questionAtom'
 
 export const ApiSubscriptionManager = () => {
-  const setQuestionsRecord = useSetAtom(questionsRecordAtom)
-  const setAnswersRecord = useSetAtom(answersRecordAtom)
   const setQnasRecord = useSetAtom(qnasRecordAtom)
   const setPollsRecord = useSetAtom(pollsRecordAtom)
   const setPollOptionsRecord = useSetAtom(pollOptionsRecordAtom)
@@ -36,24 +27,6 @@ export const ApiSubscriptionManager = () => {
           return
         }
 
-        // Load questions
-        const questionsResponse = await apiConnector.getQuestions()
-        if (questionsResponse.success && questionsResponse.data) {
-          setQuestionsRecord(questionsResponse.data)
-        } else if (!questionsResponse.success) {
-          console.error('Error loading questions:', questionsResponse.error)
-          return
-        }
-
-        // Load answers
-        const answersResponse = await apiConnector.getAnswers()
-        if (answersResponse.success && answersResponse.data) {
-          setAnswersRecord(answersResponse.data)
-        } else if (!answersResponse.success) {
-          console.error('Error loading answers:', answersResponse.error)
-          return
-        }
-
         // Load polls
         const pollsResponse = await apiConnector.getPolls()
         if (pollsResponse.success && pollsResponse.data) {
@@ -62,84 +35,16 @@ export const ApiSubscriptionManager = () => {
           console.error('Error loading polls:', pollsResponse.error)
           return
         }
-
-        // Load poll options
-        const pollOptionsResponse = await apiConnector.getPollOptions()
-        if (pollOptionsResponse.success && pollOptionsResponse.data) {
-          setPollOptionsRecord(pollOptionsResponse.data)
-        } else if (!pollOptionsResponse.success) {
-          console.error(
-            'Error loading poll options:',
-            pollOptionsResponse.error,
-          )
-          return
-        }
       } catch (err) {
         console.error('Error loading initial data:', err)
       }
     }
 
     loadInitialData()
-  }, [
-    setQuestionsRecord,
-    setAnswersRecord,
-    setQnasRecord,
-    setPollsRecord,
-    setPollOptionsRecord,
-  ])
+  }, [setQnasRecord, setPollsRecord])
 
   // Set up global subscriptions
   useEffect(() => {
-    const questionSub = apiConnector.subscribe<QuestionType>(
-      ApiMessageType.QUESTION_MESSAGE,
-      (question) => {
-        setQuestionsRecord((prev: Record<number, QuestionType>) => ({
-          ...prev,
-          [question.id]: question,
-        }))
-      },
-    )
-
-    const answerSub = apiConnector.subscribe<AnswerType>(
-      ApiMessageType.ANSWER_MESSAGE,
-      (answer) => {
-        setAnswersRecord((prev: Record<number, AnswerType>) => ({
-          ...prev,
-          [answer.id]: answer,
-        }))
-      },
-    )
-
-    const upvoteSub = apiConnector.subscribe<QuestionType | AnswerType>(
-      ApiMessageType.UPVOTE_MESSAGE,
-      (data) => {
-        // Check if it's a question or answer
-        if ('questionId' in data) {
-          // It's an answer
-          setAnswersRecord((prev: Record<number, AnswerType>) => ({
-            ...prev,
-            [data.id]: data as AnswerType,
-          }))
-        } else {
-          // It's a question
-          setQuestionsRecord((prev: Record<number, QuestionType>) => ({
-            ...prev,
-            [data.id]: data as QuestionType,
-          }))
-        }
-      },
-    )
-
-    const answeredSub = apiConnector.subscribe<QuestionType>(
-      ApiMessageType.ANSWERED_MESSAGE,
-      (question) => {
-        setQuestionsRecord((prev: Record<number, QuestionType>) => ({
-          ...prev,
-          [question.id]: question,
-        }))
-      },
-    )
-
     const pollCreateSub = apiConnector.subscribe<PollType>(
       ApiMessageType.POLL_CREATE_MESSAGE,
       (poll) => {
@@ -171,20 +76,11 @@ export const ApiSubscriptionManager = () => {
     )
 
     return () => {
-      questionSub()
-      answerSub()
-      upvoteSub()
-      answeredSub()
       pollCreateSub()
       pollVoteSub()
       pollActiveSub()
     }
-  }, [
-    setQuestionsRecord,
-    setAnswersRecord,
-    setPollsRecord,
-    setPollOptionsRecord,
-  ])
+  }, [setPollsRecord, setPollOptionsRecord])
 
   return null
 }
