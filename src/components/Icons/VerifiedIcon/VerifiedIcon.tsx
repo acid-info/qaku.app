a98f37997431482f78e1e8cb7cdf736139999958
jinhojang6 <jinho5051@gmail.com>
2025-03-20 02:32:43 +0900

    feat: add NameWithSuffix

---
+++ src/components/Icons/VerifiedIcon/VerifiedIcon.tsx
@@ -0,0 +1,19 @@
+import React, { SVGProps } from 'react'
+
+export const VerifiedIcon: React.FC<SVGProps<SVGSVGElement>> = (props) => (
+  <svg
+    width="14"
+    height="14"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g opacity="0.5">
+      <path
+        d="M7 0.583008L1.75 2.91634V6.41634C1.75 9.65384 3.99 12.6813 7 13.4163C10.01 12.6813 12.25 9.65384 12.25 6.41634V2.91634L7 0.583008ZM5.83333 9.91634L3.5 7.58301L4.3225 6.76051L5.83333 8.26551L9.6775 4.42134L10.5 5.24967L5.83333 9.91634Z"
+        fill="white"
+      />
+    </g>
+  </svg>
+)
