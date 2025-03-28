460860318a30333c240cd914fd3bc80b318f49e8
JulesFILIOT <dev@julesfiliot.com>
2025-03-28 21:46:46 +0100

    feat(QnaCreatedHeader): handle mobile style

--- src/components/QnaCreatedHeader/QnaCreatedHeader.tsx
+++ src/components/QnaCreatedHeader/QnaCreatedHeader.tsx
@@ -1,3 +1,4 @@
+import { breakpoints } from '@/configs/ui.configs'
 import styled from '@emotion/styled'
 import React, { HTMLAttributes } from 'react'
 
@@ -15,34 +16,54 @@ export const QnaCreatedHeader: React.FC<QnaCreatedHeaderProps> = ({
 }) => {
   return (
     <Wrapper {...props}>
-      <DataItem>
-        <DataItemLabel>Total Questions</DataItemLabel>
-        <DataItemValue>{questionsCount}</DataItemValue>
-      </DataItem>
-      <DataItem>
-        <DataItemLabel>Anonymous Rate</DataItemLabel>
-        <DataItemValue>{anonymousRate}%</DataItemValue>
-      </DataItem>
-      <DataItem>
-        <DataItemLabel>Named authors</DataItemLabel>
-        <DataItemValue>{namedAuthorCount}</DataItemValue>
-      </DataItem>
+      <Content>
+        <DataItem>
+          <DataItemLabel>Total Questions</DataItemLabel>
+          <DataItemValue>{questionsCount}</DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Anonymous Rate</DataItemLabel>
+          <DataItemValue>{anonymousRate}%</DataItemValue>
+        </DataItem>
+        <DataItem>
+          <DataItemLabel>Named authors</DataItemLabel>
+          <DataItemValue>{namedAuthorCount}</DataItemValue>
+        </DataItem>
+      </Content>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.div`
+  width: 100%;
+
+  @media (max-width: ${breakpoints.sm}px) {
+    overflow-x: auto;
+    display: block;
+    white-space: nowrap;
+    -webkit-overflow-scrolling: touch;
+  }
+`
+
+const Content = styled.div`
   display: flex;
   align-items: center;
   gap: 8px;
   width: 100%;
+
+  @media (max-width: ${breakpoints.sm}px) {
+    display: inline-flex;
+    min-width: 532px;
+  }
 `
+
 const DataItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   width: 100%;
 `
+
 const DataItemLabel = styled.div`
   font-size: var(--label1-font-size);
   line-height: var(--label1-line-height);
@@ -52,6 +73,7 @@ const DataItemLabel = styled.div`
   border-bottom: 1px solid var(--gray);
   width: 100%;
 `
+
 const DataItemValue = styled.div`
   font-size: var(--body1-font-size);
   line-height: var(--body1-line-height);
